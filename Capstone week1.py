9d9286cf6cd071e6681c25f638e2d136190fbdf8
Vittorio Sguazzo <vittorio.sguazzo@gmail.com>
2019-08-01 14:45:08 +0200

    First Commit

---
+++ Capstone week1.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# # Capstone week 1
+
+# ### 1. Write some markdown to explain that this notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course by Vittorio")
+
